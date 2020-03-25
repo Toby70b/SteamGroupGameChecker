f49e20f1011f7c2d317e983777642d086b3a73ef
Toby Peel <tobypl407@googlemail.com>
2020-03-25 18:42:00 +0000

    chaning package name (again)

---
+++ SGGC - webapp/src/main/java/com/sggc/controller/SGGCControllerAdvice.java
@@ -0,0 +1,49 @@
+package com.sggc.controller;
+
+
+import com.sggc.errors.ApiError;
+import com.sggc.exception.UserHasNoGamesException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.io.IOException;
+
+@RestControllerAdvice
+public class SGGCControllerAdvice extends ResponseEntityExceptionHandler {
+
+    @Value("${sggc.api.version}")
+    private String currentApiVersion;
+    private static Logger LOGGER = LoggerFactory.getLogger(SGGCControllerAdvice.class);
+    @ExceptionHandler(UserHasNoGamesException.class)
+    public ResponseEntity<ApiError> handleUserHasNoGames(UserHasNoGamesException ex) {
+
+        final ApiError error = new ApiError(
+                currentApiVersion,
+                Integer.toString(HttpStatus.NOT_FOUND.value()),
+                "User Has No Games",
+                "UserHasNoGamesException",
+                "User with Id: "+ex.getUserId()+" has no games associated with their account "
+        );
+        LOGGER.error("ERROR:", ex);
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<ApiError> handleIOException(IOException ex) {
+        final ApiError error = new ApiError(
+                currentApiVersion,
+                Integer.toString(HttpStatus.INTERNAL_SERVER_ERROR.value()),
+                "Internal Server Error",
+                "IOException",
+                "Internal server error, error within code, please check the logs..."
+        );
+        LOGGER.error("ERROR:", ex);
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
---
+++ SGGC - webapp/src/main/java/com/sggc/model/Request.java
@@ -0,0 +1,16 @@
+package com.sggc.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class Request {
+    @NonNull
+    private List<String> steamIds;
+}
